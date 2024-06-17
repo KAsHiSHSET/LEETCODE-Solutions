7c9cab4fd99d1ccc55c81a335f39d547c9a42d7c
KAsHiSHSET <139335575+KAsHiSHSET@users.noreply.github.com>
2024-06-17 08:55:45 -0700

    Time: 4 ms (40.27%), Space: 13.6 MB (37.09%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -10,14 +10,16 @@ public:
             if (nums[mid] == target) {
                 return mid;
             }
-
+               //left array sorted
             if (nums[low] <= nums[mid]) {
                 if (nums[low] <= target && target <= nums[mid]) {
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
-            } else {
+            } 
+            //right array sorted 
+            else {
                 if (nums[mid] <= target && target <= nums[high]) {
                     low = mid + 1;
                 } else {
