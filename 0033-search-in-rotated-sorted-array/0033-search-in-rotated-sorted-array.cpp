848b04f61bde297a1d5f2666f0cd793938fa7885
KAsHiSHSET <139335575+KAsHiSHSET@users.noreply.github.com>
2024-06-17 08:52:38 -0700

    Time: 8 ms (9.39%), Space: 13.6 MB (37.09%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,41 +1,4 @@
-// class Solution {
-// public:
-//     int search(vector<int>& nums, int target) {
-//         int low=0;
-//         int high=nums.size()-1;
-//         if(nums.size()==1){
-//           if(nums[0]==target){
-//             return 0;
-//           }
-//           return -1;
-//         }
-//         while(low<=high){
-//             int mid=low+(high-low)/2;
-//             if(nums[mid]==target){
-//                 return mid;
-//             }
-//             else if(nums[low]<=nums[mid]){
-//                 if(nums[low]<=target&&target<=nums[mid]){
-//                     high=mid-1;
-//                 }
-//                 else{
-//                     low=mid-1;
-//                 }
-//             }
-//             else{
-//                 if(nums[mid]<=target&&target<=nums[high]){
-//                     low=mid+1;
-//                 }
-//                 else
-//                  {
-//                     high=mid-1;
-//                  }
 
-//             }
-//         }
-//         return -1;
-//     }
-// };
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -49,13 +12,13 @@ public:
             }
 
             if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
             } else {
-                if (nums[mid] < target && target <= nums[high]) {
+                if (nums[mid] <= target && target <= nums[high]) {
                     low = mid + 1;
                 } else {
                     high = mid - 1;
