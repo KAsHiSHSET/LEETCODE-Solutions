ee731f2d942ea71053e474eaea6944f89e8e92ad
KAsHiSHSET <139335575+KAsHiSHSET@users.noreply.github.com>
2024-06-30 11:37:34 -0700

    Time: 0 ms (100%), Space: 8.1 MB (16.17%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('||s[i]=='['||s[i]=='{'){
+                st.push(s[i]);
+        
+            }
+            else if((s[i]=='}'||s[i]==']'||s[i]==')')&&st.size()!=0){
+                char ch=st.top();
+                st.pop();
+                 if((s[i]==')'&&ch=='(')||(s[i]=='}'&&ch=='{')||(s[i]==']'&&ch=='['))
+                    continue;
+                else
+                  return false;
+                
+                
+            }
+           else if((s[i]=='}'||s[i]==']'||s[i]==')')&&st.size()==0){
+             return false;
+           }
+        }
+        if(st.empty())return true;
+        else return false;
+     
+}};
