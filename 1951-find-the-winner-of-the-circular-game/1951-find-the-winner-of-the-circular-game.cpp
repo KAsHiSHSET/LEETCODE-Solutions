029d58dee1d13ceca32583f0ff87f7f8fd795d22
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2024-07-08 05:51:33 -0700

    Time: 2 ms (75.45%), Space: 7.1 MB (92.77%) - LeetHub

---
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+      int i=1;
+      for(int j=2;j<=n;j++){
+        i=i+k;
+        i=(i-1)%j+1;
+      }
+      return i;
+    }
+};
