391ecae43902938c212edd00de9ec27a518b0a4e
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2024-07-10 09:25:14 -0700

    Time: 0 ms (100%), Space: 9.1 MB (82.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,10 +12,35 @@ public:
         dp[ind]= max(pick,nonpick);
         return dp[ind];
     }
+     int tabulation(vector<int> &dp,int n,vector<int>&nums){
+        dp[0]=nums[0];
+        int pick;int nonpick;
+        for(int i=1;i<=n;i++){
+            pick=nums[i];
+            if(i>1) pick+=dp[i-2];
+             nonpick=dp[i-1];
+
+             dp[i]=max(pick,nonpick);
+        }
+        return dp[n];
+     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        int ans=solve(dp,n-1,nums);
-        return ans;
+        // vector<int> dp(n,-1);
+       // int ans=tabulation(dp,n-1,nums);
+       // return ans;
+        int prev1=nums[0]; int curr;
+        int prev2=0;//for negatives 
+        int pick;int nonpick;
+        for(int i=1;i<=n-1;i++){
+            pick=nums[i];
+            if(i>1) pick+=prev2;
+             nonpick=prev1;
+
+             curr=max(pick,nonpick);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
     }
 };
