818c2769530e3f3430c93789a383dfdfd8785792
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2024-07-11 09:30:07 -0700

    Time: 0 ms (100%), Space: 8.1 MB (17.53%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -8,9 +8,27 @@ public:
         int left=helper(i,j-1,dp);
         return dp[i][j]= (up+left);
     }
+    int tabulation(int m,int n,vector<vector<int>> &dp){
+        dp[0][0]=1;
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0&&j==0) dp[0][0]=1;
+
+                int up=0;int left=0;
+                if(i>0)
+                 up=dp[i-1][j];
+                if(j>0)
+                left=dp[i][j-1];
+                dp[i][j]=up+left;
+            }
+        }
+        return dp[m][n];
+
+    }
     int uniquePaths(int m, int n) {
       vector<vector<int>> dp(m,vector<int>(n,-1));
       int ans=  helper(m-1,n-1,dp);
+    //   int ans =tabulation(m-1,n-1,dp);
       return ans;
     }
 };
