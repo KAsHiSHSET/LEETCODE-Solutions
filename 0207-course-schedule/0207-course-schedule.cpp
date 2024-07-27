2b7b1381e87040ec4c0ec7ba12c51f5a61799f75
Kashish Seth <139335575+KAsHiSHSET@users.noreply.github.com>
2024-07-28 01:41:50 +0530

    Time: 17 ms (48.42%), Space: 17.7 MB (33.51%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        if (numCourses <= 1 || prerequisites.empty()) {
-            return true;
-        }
-        vector<vector<int>> matrix(numCourses, vector<int>(numCourses, 0));
-        vector<int> requirements(numCourses, 0);
+       vector<int> degree(numCourses,0);
+       vector<int> adj[numCourses];
+       for(auto it:prerequisites){
+         adj[it[1]].push_back(it[0]);
 
-        for (const auto& each : prerequisites) {
-            int course = each[0];
-            int pre = each[1];
-            if (matrix[pre][course] == 0) {
-                requirements[course]++;
+       }
+       for(int i=0;i<numCourses;i++){
+           for(auto ch:adj[i]){
+              degree[ch]++;
+           }
+       }
+       vector<int> ans;
+       queue<int> q;
+       for(int i=0;i<numCourses;i++){
+            if(degree[i]==0){
+                q.push(i);
             }
-            matrix[pre][course] = 1;
+       }
+       while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        ans.push_back(node);
+        for(auto it:adj[node]){
+            degree[it]--;
+            if(degree[it]==0)
+              q.push(it);
         }
-        queue<int> stack;
-        for (int i = 0; i < numCourses; ++i) {
-            if (requirements[i] == 0) {
-                stack.push(i);
-            }
-        }
-        int counter = 0;
-        while (!stack.empty()) {
-            int course = stack.front();
-            stack.pop();
-            counter++;
-            for (int i = 0; i < numCourses; ++i) {
-                if (matrix[course][i] != 0) {
-                    requirements[i]--;
-                    if (requirements[i] == 0) {
-                        stack.push(i);
-                    }
-                }
-            }
-        }
-        return counter == numCourses;
+
+       }
+       if(ans.size()==numCourses){
+        return true;
+       }
+       return false;
     }
 };
