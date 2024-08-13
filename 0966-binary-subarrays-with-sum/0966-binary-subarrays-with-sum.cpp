4ced2485c34c9d5037e61162f7c8fd873add3dbc
Kashish Seth <139335575+KAsHiSHSET@users.noreply.github.com>
2024-08-14 02:32:48 +0530

    Time: 45 ms (21.71%), Space: 38.4 MB (20.85%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int n = nums.size();
-        int count = 0;
-        int left1 = 0, left2 = 0;
-        int sum1 = 0, sum2 = 0;
-
-        for (int right = 0; right < n; ++right) {
-            sum1 += nums[right];
-            sum2 += nums[right];
-
-            // Shrink the window [left1, right] for the first sum to be equal to the goal
-            while (left1 <= right && sum1 > goal) {
-                sum1 -= nums[left1++];
-            }
-
-            // Shrink the window [left2, right] for the second sum to be equal to the goal-1
-            while (left2 <= right && sum2 >= goal) {
-                sum2 -= nums[left2++];
-            }
-
-            count += left2 - left1;
-        }
-
-        return count;
+      unordered_map<int,int> mpp;
+      int prefix=0;
+      int countsubarrays=0;
+      mpp[0]=1;
+      for(auto num:nums){
+         prefix+=num;
+         if(mpp.find(prefix-goal)!=mpp.end()){
+            countsubarrays+=mpp[prefix-goal];
+         }
+         mpp[prefix]++;
+      }
+      return countsubarrays;
     }
 };
