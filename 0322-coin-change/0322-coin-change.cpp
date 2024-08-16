c12ebae27d2d371858ceb30cc4280418f6ad4491
Kashish Seth <139335575+KAsHiSHSET@users.noreply.github.com>
2024-08-17 01:24:18 +0530

    Time: 48 ms (31.5%), Space: 47.6 MB (25.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -15,34 +15,23 @@
         
 //         int n=coins.size();
 //          vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-//         if(n==1){
-//             if(amount==0) return 0;
-//             if(amount==coins[0]) return 1;
-//             else if(amount>coins[0]) 
-//               {int g= recurse(n-1,amount,coins,dp);
-//                 if(g==1e9) return -1;
-//               }
-
-//             else return -1;
-//         }
-       
-
-//         return recurse(n-1,amount,coins,dp);
+//         int result= recurse(n-1,amount,coins,dp);
+//         return result? result>=1e9:result;
 //     }
 // };
 class Solution {
 public:
     int recurse(int n, int amount, vector<int>& coins, vector<vector<int>> &dp) {
         if (n == 0) {
-            // If we are at the first coin, we check if it can fully make up the amount
+           
             return (amount % coins[0] == 0) ? amount / coins[0] : 1e9;
         }
         if (dp[n][amount] != -1) return dp[n][amount];
         
-        int ntake = recurse(n - 1, amount, coins, dp);  // Not taking the current coin
+        int ntake = recurse(n - 1, amount, coins, dp);  
         int take = INT_MAX;
         if (coins[n] <= amount) {
-            take = 1 + recurse(n, amount - coins[n], coins, dp);  // Take the current coin
+            take = 1 + recurse(n, amount - coins[n], coins, dp);  
         }
         
         return dp[n][amount] = min(take, ntake);
@@ -53,6 +42,6 @@ public:
         vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
         
         int result = recurse(n - 1, amount, coins, dp);
-        return result >= 1e9 ? -1 : result;  // If result is large, no solution was found
+        return result >= 1e9 ? -1 : result; 
     }
 };
