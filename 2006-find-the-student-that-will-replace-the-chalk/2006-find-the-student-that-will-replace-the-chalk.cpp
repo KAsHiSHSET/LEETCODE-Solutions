d64eb26dc3224cba2cf1713dae1b508684f1dbad
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2024-09-02 10:51:47 +0530

    Time: 107 ms (15.33%), Space: 77 MB (18.08%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,37 +1,36 @@
-// class Solution {
-// public:
-//     int chalkReplacer(vector<int>& chalk, int k) {
-//        long long int sum=accumulate(chalk.begin(),chalk.end(),0LL);
-//         int total=k%sum;
-//         if(total==0) return 0;
-//         int i=0;
-//         for(;i<chalk.size();i++){
-//             if(total>chalk[i]){
-//                total=total-chalk[i];
-//             }
-//            else{
-//              break;
-//            }
-//         }
-//         return i;
-//     }
-// };
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        long long sum = accumulate(chalk.begin(), chalk.end(), 0LL);  // Accumulate chalk usage to find the total
-        k %= sum;  // Reduce k modulo the total sum to optimize the process
-
-        // Traverse through the chalk array to find the first index where k becomes less than the chalk required
-        if(k==0) return 0;
-        int i = 0;
-        for (; i < chalk.size(); i++) {
-            if (k < chalk[i]) {
-               break;
+       long long int sum=accumulate(chalk.begin(),chalk.end(),0LL);
+        int total=k%sum;
+        if(total==0) return 0;
+        int i=0;
+        for(;i<chalk.size();i++){
+            if(total>chalk[i]){
+               total=total-chalk[i];
             }
-            k -= chalk[i];
+            else if(total==chalk[i]) return i+1;
+           else{
+             break;
+           }
         }
         return i;
-
     }
 };
+// class Solution {
+// public:
+//     int chalkReplacer(vector<int>& chalk, int k) {
+//         long long sum = accumulate(chalk.begin(), chalk.end(), 0LL); 
+//         k %= sum;
+//         if(k==0) return 0;
+//         int i = 0;
+//         for (; i < chalk.size(); i++) {
+//             if (k < chalk[i]) {
+//                break;
+//             }
+//             k -= chalk[i];
+//         }
+//         return i;
+
+//     }
+// };
