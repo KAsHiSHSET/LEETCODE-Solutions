c76cd6488805f9c366d56e56426e324fd6aadc65
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2024-10-12 13:37:34 +0530

    Time: 0 ms (100%), Space: 17.9 MB (29.46%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -21,7 +21,27 @@ public:
     
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1)); // Memoization table
-        return helper(0, prices, 1, dp); // Start from index 0 with the option to buy
+        // vector<vector<int>> dp(n, vector<int>(2, -1)); // Memoization table
+        // return helper(0, prices, 1, dp); // Start from index 0 with the option to buy
+        vector<vector<long>> dpmem(n+1,vector<long>(2,0));
+        dpmem[n][0]=dpmem[n][1]=0;
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int profit = 0;
+        if(buy) {
+            // Choice to buy or skip
+            profit = max(-prices[ind] + dpmem[ind + 1][0],
+                         0 + dpmem[ind + 1][1]);
+        } else {
+            // Choice to sell or skip
+            profit = max(prices[ind] + dpmem[ind + 1][1], 
+                         0 + dpmem[ind + 1][0]);
+        }
+        
+      dpmem[ind][buy] = profit; // Store result in dp array 
+            }
+        }
+
+   return dpmem[0][1];
     }
 };
