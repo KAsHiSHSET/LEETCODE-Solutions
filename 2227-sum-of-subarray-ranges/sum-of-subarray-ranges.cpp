435a4a90869b023e881d59570fb35d5a81d97f4b
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-04-22 16:06:53 +0530

    Time: 7 ms (46.74%) | Memory: 14 MB (88.51%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,3 +1,21 @@
+// class Solution {
+// public:
+//     long long subArrayRanges(vector<int>& nums) {
+//         int n=nums.size();
+//         long long int sum=0;
+//         int mod= 1e9 + 7 ;
+//         for(int i=0;i<n;i++){
+//             int mini=nums[i];int maxi=nums[i];
+//             for(int j=i;j<n;j++){
+//                 mini=min(mini,nums[j]);
+//                 maxi=max(maxi,nums[j]);
+//                 sum=(sum+((maxi-mini+mod)%mod))%mod;
+
+//             }
+//         }
+//         return sum;
+//     }
+// };
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
