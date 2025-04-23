c091b16afeb9c55662b8727beb23e11bdbae702e
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-04-23 11:56:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (57.97%) - LeetSync

--- 1500-count-largest-group/count-largest-group.cpp
+++ 1500-count-largest-group/count-largest-group.cpp
@@ -10,6 +10,12 @@ public:
         }
         return sum;
     }
+    //tc is 0(nlogn)
+    //sc is 0(1) as max digit sum can be 36=(9+9+9+9)
+ 
+    //find the sum of all digits, maintain a map 
+    //then increase the frequency of the sum of digit we must gave encountered,then find the maximum no 
+    //then find for maximum of the frequency how many such digits exists.
     int countLargestGroup(int n) {
         //1->13
         int maxi=0;
