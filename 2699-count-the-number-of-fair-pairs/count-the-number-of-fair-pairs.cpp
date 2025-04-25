7f68a1fe8676bd4dc10d4f66690c78c77e3dd36c
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-04-25 16:59:26 +0530

    Time: 67 ms (11.76%) | Memory: 71.7 MB (5.34%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         long long result=0;
+        //tc- 0(N log N)
+        //lower and upper bound are binary search with tcs of log n 
         sort(begin(nums),end(nums));
         for(int i=0;i<nums.size();i++){
-            
+            //upper bound returns an iterator to the first element that is greater than  the given value.
+            //lower bound returns an iterator to the first element that is not less than (i.e., ≥) the given value.
             int l= lower_bound(begin(nums)+i+1,end(nums),lower-nums[i])-begin(nums)-1;
             int x= l-i;
             int u=upper_bound(begin(nums)+i+1,end(nums),upper-nums[i])-begin(nums)-1;
