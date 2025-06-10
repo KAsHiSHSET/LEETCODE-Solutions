c2bb5b5d780223c344620210fcbf629baa292ba7
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-06-10 14:22:17 +0530

    Time: 0 ms (100%), Space: 13.6 MB (76.63%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,18 +4,19 @@ public:
        int low=0;
        int high=arr.size()-1;
        int ans=arr.size();
+
        while(low<=high){
-        int mid=low+(high-low)/2;
-        if(arr[mid]>target){
+        int mid= low+(high-low)/2;
+        if(arr[mid]==target) return mid;
+        else if(arr[mid]>=target){
+            ans=mid;
             high=mid-1;
         }
-        else if(arr[mid]<target){
+        else{
             low=mid+1;
         }
-        else{
-            return mid;
-        }
        }
-       return high+1;
+        return high+1;
     }
+    
 };
