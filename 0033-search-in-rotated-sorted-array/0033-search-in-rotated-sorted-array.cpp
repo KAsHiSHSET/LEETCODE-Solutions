29eb4e0206b4df87d782086b66324e37ab45199e
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-06-10 16:46:23 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.57%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -19,13 +19,14 @@ public:
                 }
             } 
             //right array sorted 
-            else {
+            else if(nums[mid]<=nums[high]) {
                 if (nums[mid] <= target && target <= nums[high]) {
                     low = mid + 1;
                 } else {
                     high = mid - 1;
                 }
             }
+           
         }
 
         return -1;
