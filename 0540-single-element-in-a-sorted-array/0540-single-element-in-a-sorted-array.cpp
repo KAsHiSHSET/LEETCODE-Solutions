d3dee272f900ad030ffda13a60f74634612f8382
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-06-12 14:09:37 +0530

    Time: 0 ms (100%), Space: 26.2 MB (33.48%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,3 +1,34 @@
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+//          if(nums.size()==1){
+//             return nums[0];
+//         }
+//         int low=1;
+//         int high=nums.size()-1;
+//         if(nums[0]!=nums[1]){
+//             return nums[0];
+//         }
+//         if(nums[nums.size()-1]!=nums[nums.size()-2]){
+//             return nums[nums.size()-1];
+//         }
+//         while(low<=high){
+//             int mid=low+(high-low)/2;
+//             if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]){
+//                 return nums[mid];
+//             }
+//             //eleimine the left half
+//             if((mid%2!=0&&nums[mid-1]==nums[mid])||(mid%2==0&&nums[mid+1]==nums[mid])){
+//                 low=mid+1;
+//             }
+//             //eleiminate the right half
+//             else{
+//                 high=mid-1;
+//             }
+//         }
+//         return -1;
+//     }
+// };
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
@@ -17,11 +48,11 @@ public:
             if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]){
                 return nums[mid];
             }
-            //eleimine the left half
+            //you are in left half and eleminate the left half
             if((mid%2!=0&&nums[mid-1]==nums[mid])||(mid%2==0&&nums[mid+1]==nums[mid])){
                 low=mid+1;
             }
-            //eleiminate the right half
+            //you are in right half and eleiminate the right half
             else{
                 high=mid-1;
             }
