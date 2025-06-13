6cc2eeba2925b050e5e77be16aa05df1addeb40c
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-06-13 15:22:58 +0530

    Time: 55 ms (49.19%), Space: 18.8 MB (38%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,3 +1,7 @@
+// Time Complexity: O(N+M), where N = given row number, M = given column number.
+// Reason: We are starting traversal from (0, M-1), and at most, we can end up being in the cell (N-1, 0). So, the total distance can be at most (N+M). So, the time complexity is O(N+M).
+
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
