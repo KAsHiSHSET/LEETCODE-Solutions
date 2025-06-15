db02c36857f790d10f3ce4de865e386f5b3b080d
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-06-15 21:13:07 +0530

    Time: 0 ms (100%), Space: 8.6 MB (35.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,7 +5,7 @@ public:
         // if(n==1) return 1;
         //  else{
         //    return  climbStairs(n-1)+climbStairs(n-2);
-        vector<int> dp(n+1);
+        vector<int> dp(n+1,-1);
         dp[0]=1;
         dp[1]=1;
         for(int i=2;i<=n;i++){
@@ -13,5 +13,9 @@ public:
         }
         return dp[n];
          }
+        //  if(n==0||n==1) return 1;
+        //   if(dp[n]!=-1) return dp[n];
+        //   return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+         
     
 };
