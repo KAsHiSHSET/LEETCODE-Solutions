e8d78f6d921aec21d2ada92080c9f4949f1ff15e
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-06-17 21:53:32 +0530

    Time: 0 ms (100%), Space: 9.5 MB (9.04%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -10,25 +10,26 @@ public:
     }
     int tabulation(int m,int n,vector<vector<int>> &dp){
         dp[0][0]=1;
-        for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0&&j==0) dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0) continue;
 
                 int up=0;int left=0;
                 if(i>0)
-                 up=dp[i-1][j];
+                up=dp[i-1][j];
                 if(j>0)
                 left=dp[i][j-1];
                 dp[i][j]=up+left;
             }
         }
-        return dp[m][n];
+        return dp[m-1][n-1];
 
     }
     int uniquePaths(int m, int n) {
       vector<vector<int>> dp(m,vector<int>(n,-1));
-      int ans=  helper(m-1,n-1,dp);
-    //   int ans =tabulation(m-1,n-1,dp);
-      return ans;
+    //   int ans=  helper(m,n,dp);
+    int ans ;
+    return  ans =tabulation(m,n,dp);
+  
     }
 };
