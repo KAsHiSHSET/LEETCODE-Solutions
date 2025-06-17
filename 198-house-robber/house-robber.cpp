1d47694043cc2334a4159b48b2943678e5a1b623
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-06-17 11:06:18 +0530

    Time: 2 ms (7.53%) | Memory: 10.7 MB (22.30%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    int helper(int ind,vector<int> &nums,vector<int> &dp){
-        if(ind==0) return nums[0];
-        if(ind <0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-       int pick=nums[ind]+helper(ind-2,nums,dp);
-       int nonpick=helper(ind-1,nums,dp);
-        return dp[ind]=max(pick,nonpick);
+    int helper(vector<int> &nums){
+    //     if(ind==0) return nums[0];
+    //     if(ind <0) return 0;
+    //     if(dp[ind]!=-1) return dp[ind];
+    //    int pick=nums[ind]+helper(ind-2,nums,dp);
+    //    int nonpick=helper(ind-1,nums,dp);
+    //     return dp[ind]=max(pick,nonpick);
+    int n= nums.size();
+    vector<int> dp(n,0);
+    dp[0]=nums[0];
+    for(int i=1;i<n;i++){
+    int pick=nums[i];
+    if(i>1)
+    {pick+=dp[i-2];}
+    int nonpick=dp[i-1];
+    dp[i]= max(pick,nonpick);
+
+    }
+    return dp[n-1];
 
     }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> dp(n,-1);
+        // int n=nums.size();
+        // vector<int> dp(n,-1);
     
-        return helper(n-1,nums,dp);
+        return helper(nums);
     }
 };
