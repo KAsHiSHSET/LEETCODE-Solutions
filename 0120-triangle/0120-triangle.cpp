2ed9b3c4fe60391fe08d9d05bfaf21630face378
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-06-18 12:08:38 +0530

    Time: 0 ms (100%), Space: 13.2 MB (30.95%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -11,7 +11,19 @@ public:
         int m=triangle.size();
         int n=m;
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        int ans=helper(triangle,0,0,dp);
-        return ans;
+       // int ans=helper(triangle,0,0,dp);
+       // return ans;
+       for(int j=0;j<n;j++){
+        dp[m-1][j]=triangle[m-1][j];
+        
+       }
+      for(int i= m-2;i>=0;i--){
+        for(int j=i;j>=0;j--){
+            int diag = dp[i+1][j+1];
+            int down=dp[i+1][j];
+            dp[i][j]= triangle[i][j]+min(diag,down);
+        }
+      }
+      return dp[0][0];
     }
 };
