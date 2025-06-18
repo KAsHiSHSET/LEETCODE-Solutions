ad72a2fcc9932a65a98c5702f1e5ca31a52dc910
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-06-18 11:39:52 +0530

    Time: 0 ms (100%), Space: 16.1 MB (71.6%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -13,7 +13,23 @@ public:
         int n=grid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
         int ans;
-        return ans=helper(m-1,n-1,grid,dp);
+        // return ans=helper(m-1,n-1,grid,dp);
+        //for tabulation doing base case first.
+        dp[0][0]=grid[0][0];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+            if(i==0 && j==0) continue;
+            int left=1e9; int right=1e9;
+            if(j>0)
+              left = dp[i][j-1];
+            if(i>0)
+              right = dp[i-1][j];
+            dp[i][j]=grid[i][j]+min(left,right);
+            }
+        }
+        return dp[m-1][n-1];
+        
     }
 
 };
