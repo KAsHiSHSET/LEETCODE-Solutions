5c4c4d6a9e9243d6446f67d56c02ba8f00f5a954
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-06-18 15:06:39 +0530

    Time: 0 ms (100%), Space: 14.4 MB (86.18%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int helper(int i,int j,vector<vector<int>>& dp,vector<vector<int>>&matrix){
-        if(j<0||j>=matrix.size()) return 1e4;
+        //base cases
+        if(j<0||j>=matrix[0].size()) return 1e4;
         if(i==0) return matrix[0][j];
         if(dp[i][j]!=-1) return dp[i][j];
         int down=helper(i-1,j,dp,matrix);
@@ -21,28 +22,30 @@ public:
         //         int z=helper(m-1,j,dp,matrix);
         //         mini=min(mini,z);
         //     }
-        //tabulataion->0 becoz pichhle waale sum la rahe honge and -1 reduces sum
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        //tabulataion->0 becoz pichhle waale sum la rahe honge and -1 reduces sum   
+        vector<int> prev(n,0),curr(n,0);
+        // vector<vector<int>> dp(m,vector<int>(n,0));
         for(int j=0;j<n;j++){
-            dp[0][j]=matrix[0][j];
+            prev[j]=matrix[0][j];
         }   int mini=1e4;
        
         for(int i=1;i<m;i++ ){
             for(int j=0;j<n;j++){
-                int down=matrix[i][j]+dp[i-1][j];
+                int down=matrix[i][j]+prev[j];
                  int leftdiag=matrix[i][j];
-                  if(j-1>=0) leftdiag+=dp[i-1][j-1];
+                  if(j-1>=0) leftdiag+=prev[j-1];
                  else leftdiag+=mini;
                    int  rightdiag=matrix[i][j];
-                  if(j+1<n) rightdiag+=dp[i-1][j+1];
+                  if(j+1<n) rightdiag+=prev[j+1];
                   else rightdiag+=mini;
-                  dp[i][j]=min(down,min(leftdiag,rightdiag));
+                  curr[j]=min(down,min(leftdiag,rightdiag));
             }
+            prev=curr;
         }
        
         for(int j=0;j<n;j++){
                 
-                mini=min(mini,dp[m-1][j]);
+                mini=min(mini,prev[j]);
             }
             return mini;
     }
