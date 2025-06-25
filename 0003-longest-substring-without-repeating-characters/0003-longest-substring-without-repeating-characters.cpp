f3796c8d12112a7b31b0e951921a262ed39f9145
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-06-25 12:29:46 +0530

    Time: 18 ms (32.74%), Space: 14.3 MB (22.46%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,23 +2,39 @@ class Solution {
 public:
 
     int lengthOfLongestSubstring(string s) {
-        int n = s.length();
-        unordered_map<char, int> hash;
-        int left = 0;
-        int right = 0;
-        int maxLen = 0;
-        while(right < n){
+        // int n = s.length();
+        // unordered_map<char, int> hash;
+        // int left = 0;
+        // int right = 0;
+        // int maxLen = 0;
+        // while(right < n){
            
-            if(hash.find(s[right])==hash.end() || hash[s[right]] < left){
-                hash[s[right]] = right;
-                maxLen = max(maxLen, right-left+1);
-                right++;       
-            }
+        //     if(hash.find(s[right])==hash.end() || hash[s[right]] < left){
+        //         hash[s[right]] = right;
+        //         maxLen = max(maxLen, right-left+1);
+        //         right++;       
+        //     }
         
-            else{
-                left = hash[s[right]]+1;
+        //     else{
+        //         left = hash[s[right]]+1;
+        //     }
+        // }
+        // return maxLen;
+        if(s.length()==0) return 0;
+        int left=0;
+        int maxans=INT_MIN;
+        unordered_set<int> st;
+        for(int right=0;right< s.length();right++){
+
+           if(st.find(s[right])!=st.end()){
+            while(left<right&&st.find(s[right])!=st.end()){
+                st.erase(s[left]);
+                left++;
             }
+           }
+            st.insert(s[right]);
+            maxans=max(maxans,right-left+1);
         }
-        return maxLen;
+        return maxans;
     }
 };
