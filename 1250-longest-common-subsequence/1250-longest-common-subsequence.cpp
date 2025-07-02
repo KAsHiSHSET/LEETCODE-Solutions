92b385a96e11fd64035417f8648f3e43cd272a9d
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-07-02 17:34:08 +0530

    Time: 14 ms (93.64%), Space: 8.9 MB (99.8%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -56,25 +56,28 @@ public:
     int longestCommonSubsequence(const string& text1, const string& text2) {
         int m = text1.length();
         int n = text2.length();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        vector<int> prev(n+1,0);
+        vector<int> curr(n+1,0);
+        // vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
         // return helper(m, n, text1, text2, dp);
-        for(int i=0;i<=m;i++){
-            dp[i][0]= 0;
+        // for(int i=0;i<=m;i++){
+        //     dp[i][0]= 0;
            
-        }
-        for(int i=0;i<=n;i++){
-             dp[0][i]=0;
-        }
+        // }
+        // for(int i=0;i<=n;i++){
+        //      dp[0][i]=0;
+        // }
         for(int i=1;i<=text1.size();i++){
             for(int j=1;j<=text2.size();j++){
                 if(text1[i-1]==text2[j-1]){
-                    dp[i][j]=1+ dp[i-1][j-1];
+                    curr[j]=1 + prev[j-1];
                 }
                 else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                   curr[j]=max(prev[j],curr[j-1]);
                 }
             }
+            prev=curr;
         }
-        return dp[m][n];
+        return prev[n];
     }
 };
