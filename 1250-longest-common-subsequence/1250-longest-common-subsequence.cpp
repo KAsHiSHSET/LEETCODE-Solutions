2e7bb19df0ca8d2afde14af93a09e7973b664a5d
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-07-02 17:10:22 +0530

    Time: 42 ms (25.33%), Space: 27.4 MB (50.7%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -44,9 +44,9 @@ class Solution {
 public:
     // Pass the vector by reference
     int helper(int i, int j, const string& text1, const string& text2, vector<vector<int>>& dp) {
-        if (i == 0 || j== 0) return 0;
+        if (i < 0 || j < 0) return 0;
         if (dp[i][j] != -1) return dp[i][j];
-        if (text1[i-1] == text2[j-1]) {
+        if (text1[i] == text2[j]) {
             return dp[i][j] = 1 + helper(i - 1, j - 1, text1, text2, dp);
         }
         
@@ -56,7 +56,7 @@ public:
     int longestCommonSubsequence(const string& text1, const string& text2) {
         int m = text1.length();
         int n = text2.length();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return helper(m, n, text1, text2, dp);
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return helper(m-1, n-1, text1, text2, dp);
     }
 };
