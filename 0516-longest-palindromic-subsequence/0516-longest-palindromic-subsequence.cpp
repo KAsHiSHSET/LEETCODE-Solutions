6c502336a906fedf3d1e56124f8886a354c15910
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-07-03 23:42:14 +0530

    Time: 88 ms (42.17%), Space: 75.9 MB (62.43%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
-    int recurse(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
-        if (i < 0 || j < 0) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (s1[i] == s2[j])
-            return dp[i][j] = 1 + recurse(i - 1, j - 1, s1, s2, dp);
-        else
-            return dp[i][j] = max(recurse(i - 1, j, s1, s2, dp), recurse(i, j - 1, s1, s2, dp));
+    int recurse(int i,int j,string s1,string s,vector<vector<int>> &dp){
+        if(i==0||j==0){
+            return 0;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i-1]==s[j-1]){
+         return dp[i][j]= 1+ recurse(i-1,j-1,s1,s,dp);
+        }
+        else{
+            return dp[i][j]= max(recurse(i-1,j,s1,s,dp),recurse(i,j-1,s1,s,dp));
+        }
     }
-
     int longestPalindromeSubseq(string s) {
-        string s1 = s;
-        reverse(s.begin(), s.end());
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));  // Use size n x n
-        return recurse(n - 1, n - 1, s1, s, dp);
+        string s1=s;
+       reverse(s.begin(),s.end());
+        int n=s1.length();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        // return recurse(n,n,s1,s,dp);
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s[j-1]){
+                    dp[i][j]=1+ dp[i-1][j-1];
+                }
+                else{
+
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
     }
 };
