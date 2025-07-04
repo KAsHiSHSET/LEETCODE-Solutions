a5ecbe601eee19a20d8bcd0213695b0a6c55e8d5
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-07-05 00:37:12 +0530

    Time: 435 ms (32.49%), Space: 235.4 MB (7.34%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,26 +2,29 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<long>>> dp(n+1, vector<vector<long>>(2, vector<long>(3, 0)));
+        vector<vector<vector<long>>> dp(n+1, vector<vector<long>>(2, vector<long>(3, 0))); 
+        vector<vector<long>> ahead(2,vector<long>(3,0));
+        vector<vector<long>>  curr(2,vector<long>(3,0));
         
         // Base case initialization is handled implicitly as dp is initialized with 0
         
         // Fill the DP table from the end to the start (bottom-up)
-        for (int i = n - 1; i >= 0; --i) {
-            for (int buy = 0; buy < 2; ++buy) {
-                for (int cap = 1; cap < 3; ++cap) {  // No need to process cap = 0 (as it's 0 by default)
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy < 2; buy++) {
+                for (int cap = 1; cap < 3; cap++) {  // No need to process cap = 0 (as it's 0 by default)
                     if (buy) {
                         // Choice to buy or skip
-                        dp[i][buy][cap] = max(-prices[i] + dp[i+1][0][cap], dp[i+1][1][cap]);
+                       curr[buy][cap] = max(-prices[i] + ahead[0][cap],0 + ahead[1][cap]);
                     } else {
                         // Choice to sell or skip
-                        dp[i][buy][cap] = max(prices[i] + dp[i+1][1][cap-1], dp[i+1][0][cap]);
+                        curr[buy][cap] = max(prices[i] + ahead[1][cap-1], ahead[0][cap]);
                     }
                 }
             }
+            ahead=curr;
         }
         
         // The result is the maximum profit starting from day 0, with the ability to buy, and 2 transactions available
-        return dp[0][1][2];
+        return curr[1][2];
     }
 };
