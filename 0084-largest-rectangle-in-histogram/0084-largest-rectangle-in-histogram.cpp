8670138c564dd060d9dbe03fbf43fddcc89f9614
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-07-11 14:37:41 +0530

    Time: 15 ms (87.41%), Space: 79.4 MB (97.16%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -5,20 +5,27 @@ public:
         int ans=0;
      
         stack<int> st;
-        for(int i=0;i<=n;i++){
-        while((!st.empty())&&(i==n||heights[st.top()]>=heights[i])){
+        for(int i=0;i<n;i++){
+            //to maintain the stack increasing , if we encounter the smaller element than the stack top , pop the elements from top of the stack 
+        while((!st.empty())&&(heights[st.top()]>=heights[i])){
                int height=heights[st.top()];
                st.pop();
                int width;
-               if(st.empty()) width=i;
-               else
-               width=i-st.top()-1;
+              if(st.empty()) width=i-0;
+              else
+                 width=i-st.top()-1;
           
 
 
           ans=max(ans,height*width);
         }
         st.push(i);
+        }
+        while(!st.empty()){
+            int nse=heights.size();
+         int element= st.top(); st.pop();
+            int pse=st.empty()?-1:st.top();
+            ans=max(ans,heights[element]*(nse-pse-1));
         }
           return ans;
     }
