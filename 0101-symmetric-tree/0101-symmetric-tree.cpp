0871b82b1852de2b6ac700a46dcb9ff6b6b2906a
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-07-14 21:28:42 +0530

    Time: 0 ms (100%), Space: 18.5 MB (61.15%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -9,14 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //tc -> 0(n)
+//  val equality
+
+// left-left vs right-right
+
+// right-left vs left-right
+
+//sc -> for skewed unbalnec its 0(n)
+//for 0(log n)
 class Solution {
 public:
     bool helper(TreeNode* left,TreeNode* right){
         if(left==NULL||right==NULL) return left==right;
+        // the node i m siting on , we check the value and then check the equlity
         return (left->val==right->val)&&helper(left->left,right->right)&&helper(left->right,right->left);
     }
     bool isSymmetric(TreeNode* root) {
         if (root==NULL) return true;
+        //we hit call on lft and right of root.
         return helper(root->left,root->right);
     }
 };
