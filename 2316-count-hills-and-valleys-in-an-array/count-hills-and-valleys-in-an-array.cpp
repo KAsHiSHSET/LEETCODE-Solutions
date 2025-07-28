e75e31862c689408abcdd9b26de5d5590f993fde
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-07-28 23:31:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (50.62%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -7,11 +7,12 @@ public:
         int count=0;
         while(j+1<n){
             //peak or valley
+            //if we have found , move i after counting 
             if((nums[i]>nums[j] && nums[j]<nums[j+1]) ||(nums[i]<nums[j] && nums[j]>nums[j+1])){
                 count++;
                 i=j;
             }
-            //if its equal move aaage
+            //if its equal move j in front , to compare. 
          
             j++;
         }
