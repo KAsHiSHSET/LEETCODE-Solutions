ca49914154fd3c53ff62cfe6331270a9f2f810c4
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-07-31 23:26:23 +0530

    Time: 1887 ms (8.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+ select s.name
+from SalesPerson s
+where s.name not in
+    (select s.name
+    from SalesPerson s
+        left join Orders o on s.sales_id = o.sales_id
+        left join Company c on o.com_id = c.com_id
+    where c.name = 'Red')
+--     1. FROM SalesPerson s
+-- You start with the SalesPerson table.
+
+-- 2. LEFT JOIN Orders o ON s.sales_id = o.sales_id
+-- You bring in matching orders made by each salesperson (if any).
+
+-- If a salesperson has no orders, you still keep the row — with NULL in Orders.
+
+-- 3. LEFT JOIN Company c ON o.com_id = c.com_id
+-- For each order, you attach the company info.
+
+-- If the order's com_id doesn't match any company, c.name will be NULL.
+-- If c.name is NULL (i.e., no matching company or no order), that row will not satisfy c.name = 'Red' → and thus be filtered out.
