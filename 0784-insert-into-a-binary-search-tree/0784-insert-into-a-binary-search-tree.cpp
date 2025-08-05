8e7ba59a4fa9910d8bfd04aa4c6eb85b14f5c30a
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-08-05 16:50:49 +0530

    Time: 0 ms (100%), Space: 59.5 MB (8.64%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
@@ -2,27 +2,48 @@ class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
          TreeNode* newnode = new TreeNode(val);
+        
+        // if(root==nullptr)
+        // {
             
-        if(root==nullptr)
-        {
+        //     return newnode;
+        // }
+        // if (root->left == nullptr && root->right == nullptr) {
             
-            return newnode;
+        //     if (root->val > val) {
+        //         root->left = newnode;
+        //     } else {
+        //         root->right = newnode;
+        //     }
+        //     return root;
+        // }
+        // if (root->val > val) {
+        //     root->left = insertIntoBST(root->left, val);
+        // } else {
+        //     root->right = insertIntoBST(root->right, val);
+        // }
+
+        // return root;
+        if(root==NULL) return newnode;
+        //base-case : reached last node.
+        else if(root->left == nullptr && root->right == nullptr){
+        if(root->val>val){
+            root->left=newnode;
         }
-        if (root->left == nullptr && root->right == nullptr) {
-            
-            if (root->val > val) {
-                root->left = newnode;
-            } else {
-                root->right = newnode;
-            }
-            return root;
+        else{
+            root->right =newnode;
+
         }
+        return root;}
+        //reaching the approproate place
         if (root->val > val) {
             root->left = insertIntoBST(root->left, val);
         } else {
             root->right = insertIntoBST(root->right, val);
         }
+         //after inserting on approrpirate place //last tak ghhom ke aaake waapas aaagaye ,
+         //now returning root
+         return root;
 
-        return root;
     }
 };
