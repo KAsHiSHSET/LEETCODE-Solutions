404f06d198a3f0f669f4cd8608889a5559d20981
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-08-15 14:02:19 +0530

    Time: 6 ms (20.33%), Space: 17.8 MB (95.96%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -7,13 +7,16 @@ public:
         }
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> min_heap;
         for(auto it:mpp){
+            //put in a map , according to the frequency and element.
             min_heap.push({it.second,it.first});
+            //if the size is greater than k , then pop the minimum  elemeents from top
             if(min_heap.size()>k){
                 min_heap.pop();
             }
         }
         vector<int> ans;
         while(min_heap.size()!=0){
+            //push the eleemnts in the answer array
             ans.push_back(min_heap.top().second);
             min_heap.pop();
         }
