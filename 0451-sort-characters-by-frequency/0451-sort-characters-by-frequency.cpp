f371bcb5f0fa41b229430673ebc5e80050038faa
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-08-16 01:19:18 +0530

    Time: 3 ms (54.04%), Space: 10.7 MB (88.73%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,3 +1,29 @@
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         unordered_map<char,int> mp;
+//         for(auto ch:s){
+//             mp[ch]++;
+//         }
+//         //tc - > 0(nlogn)
+//         vector<pair<char,int>> vec(mp.begin(),mp.end());
+//         sort(vec.begin(),vec.end(),[](auto &a,auto &b){
+//             return a.second > b.second;
+//         });  
+//         // If a.second is greater than b.second, a should come before b in the sorted order.
+//         // So, bigger frequency → comes earlier in the list.
+//         string result="";
+//         for(auto ch:vec){
+//             result.append(ch.second,ch.first);
+//         }
+//         return result;
+//     }
+// };
+// You push k elements into a priority_queue (max heap).
+
+// Each push takes O(log k) time (heapify cost).
+
+// Total: O(k log k)
 class Solution {
 public:
     string frequencySort(string s) {
@@ -5,16 +31,16 @@ public:
         for(auto ch:s){
             mp[ch]++;
         }
-        vector<pair<char,int>> vec(mp.begin(),mp.end());
-        sort(vec.begin(),vec.end(),[](auto &a,auto &b){
-            return a.second > b.second;
-        });  
-        // If a.second is greater than b.second, a should come before b in the sorted order.
-        // So, bigger frequency → comes earlier in the list.
-        string result="";
-        for(auto ch:vec){
-            result.append(ch.second,ch.first);
+    priority_queue<pair<int,char>> max_heap;
+        for (auto [ch, freq] : mp) {
+           max_heap.push({freq, ch});
         }
+        //max_heap -> highest element on the top
+        string result=""; 
+        while(!max_heap.empty()){ 
+          for(int i=0;i<max_heap.top().first;i++)
+         result+=max_heap.top().second; 
+          max_heap.pop(); }
         return result;
     }
 };
