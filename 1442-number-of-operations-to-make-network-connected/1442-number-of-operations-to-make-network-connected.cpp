934a3a04133932377122df3e5181b5d51e4cb0bd
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-08-28 19:53:08 +0530

    Time: 21 ms (46.87%), Space: 47.2 MB (28.36%) - LeetHub

---
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,90 @@
+//we will make a disjoint set union
+class disjoint{
+    
+    public:
+    vector<int> rank,parent, size;
+    //calling a constructor 
+    disjoint(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+    //member functions of claass
+    int findupar(int node){
+        if(node==parent[node]){
+            return node;
+        }
+         parent[node] = findupar(parent[node]);
+         return parent[node];
+    }
+    void unionbyrank(int u,int v){
+        int ulp_u = findupar(u);
+        int ulp_v= findupar(v);
+        if(ulp_u==ulp_v) return;
+        if(rank[ulp_v]<rank[ulp_u]){
+            parent[ulp_u]=ulp_v;
+        }
+        else if(rank[ulp_v]<rank[ulp_u]){
+            parent[ulp_v]=ulp_u;
+
+        }
+        else{
+            //both ranks are same , attach any one to any other 
+            parent[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionbysize(int u,int v){
+        int ulp_u = findupar(u);
+        int ulp_v= findupar(v);
+        if(ulp_u==ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+    parent[ulp_u] = ulp_v;
+}
+else if(rank[ulp_v] < rank[ulp_u]) {
+    parent[ulp_v] = ulp_u;
+}
+else {
+    parent[ulp_v] = ulp_u;
+    rank[ulp_u]++;
+}
+
+    }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        //this is the question on dsu
+         disjoint ds(n);
+         int extraedge=0;
+         int parentcomp=0;
+         for(auto it: connections){
+            int u = it[0];
+            int v= it[1];
+            if(ds.findupar(u)==ds.findupar(v)){
+                //if they have same parent then count the extraedge
+                extraedge++;
+            }
+            else{
+                //if not the ultimate parent then you should make them unite
+                ds.unionbysize(u,v);
+            }
+
+         }
+         // ideallly 
+        //  parentcomp - 1 = extraedgs
+        for(int i=0;i<n;i++){
+    if(ds.parent[i]==i){
+        parentcomp++;
+    }
+}
+
+         int ans = parentcomp-1;
+         if(extraedge>= ans ) return ans;
+         else {return -1;}
+
+    }
+};
