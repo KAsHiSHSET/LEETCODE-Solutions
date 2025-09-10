8d3fe25d6244efa0a0f0934e7872b5beb992bfca
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-09-10 18:01:14 +0530

    Time: 9 ms (33.73%), Space: 11 MB (18.8%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -9,7 +9,7 @@ public:
         for(int r=0;r<s.length();r++){
            mp[s[r]]++;
            maxfreq=max(maxfreq,mp[s[r]]);
-           while((r-l+1-maxfreq)>k){
+            if((r-l+1-maxfreq)>k){
             mp[s[l]]--;
             l++;
            }
