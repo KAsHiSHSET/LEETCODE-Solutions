ae48800fad8b156b0d211db9b4a2d5f7a5c54a75
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-09-11 23:58:41 +0530

    Time: 4 ms (88.2%), Space: 44.9 MB (43.13%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int i=0; int ans=0;
-        sort(s.begin(),s.end());
+        int i=0;
+        int ans=0;
+        //in greedy always sort 
         sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
         for(int j=0;j<s.size();j++){
-            if(i<g.size()&&g[i]<=s[j]){
+            if(i<g.size() && g[i]<=s[j]){
                 ans++;
                 i++;
-            
             }
         }
         return ans;
