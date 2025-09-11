591e08cbcf952f3e83b25b0d0b11e1cd35bfd102
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-09-11 16:55:06 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (45.47%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -10,7 +10,7 @@ public:
           i++;
        }
        //while we habe ovrlapping  , keep on updating the interval
-       while(i<n&&intervals[i][0]<=newInterval[1]){
+       while(i<n&& intervals[i][0]<=newInterval[1] && intervals[i][1]>=newInterval[0]){
         //updating the value and expanding the range 
         newInterval[0]=min(newInterval[0],intervals[i][0]);
         newInterval[1]=max(newInterval[1],intervals[i][1]);
