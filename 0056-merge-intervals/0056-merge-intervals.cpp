17690500c9fbcd16c192fe0209fa44e700eeb33b
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-09-11 17:27:07 +0530

    Time: 341 ms (5.07%), Space: 23.3 MB (98.91%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-       vector<vector<int>> ans;
-       //merge the overlapping intervals
+    //    vector<vector<int>> ans;
+    //    //merge the overlapping intervals
        sort(intervals.begin(),intervals.end());
-       for(auto interval:intervals){
-        if(ans.empty()||ans.back()[1]<interval[0]){
-            ans.push_back(interval);
-        }
-        else if(ans.back()[1]>=interval[0]){
-            ans.back()[1]=max(ans.back()[1],interval[1]);
-        }
+    //    for(auto interval:intervals){
+    //     if(ans.empty()||ans.back()[1]<interval[0]){
+    //         ans.push_back(interval);
+    //     }
+    //     else if(ans.back()[1]>=interval[0]){
+    //         ans.back()[1]=max(ans.back()[1],interval[1]);
+    //     }
      
-       }
-         return ans ;
+    //    }
+    //      return ans ;
+    int i=0;
+    while(i<intervals.size()-1){
+           if(intervals[i][1]>=intervals[i+1][0]){
+                intervals[i][0]=min(intervals[i][0],intervals[i+1][0]);
+                intervals[i][1]=max(intervals[i][1],intervals[i+1][1]);
+                
+                intervals.erase(intervals.begin() + i + 1);
+          }
+          else{
+            i++;
+          }
+    }
+    return intervals;
     }
 };
