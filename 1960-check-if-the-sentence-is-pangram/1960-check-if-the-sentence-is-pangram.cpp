ca12c2e011082d3ed2feb505fb417d74176e2a73
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-09-15 21:58:31 +0530

    Time: 0 ms (100%), Space: 8.9 MB (35.65%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
@@ -4,5 +4,8 @@ public:
        
          unordered_set<char> st(sentence.begin(),sentence.end());
         return st.size()==26;
+        //tc-> 0(N) for best case
+        
+        //sc-> 0(26)
     }
 };
