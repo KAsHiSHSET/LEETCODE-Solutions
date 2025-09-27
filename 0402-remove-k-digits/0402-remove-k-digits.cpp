8224281a4b900ea232e8175af504c296fc14729f
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-09-27 17:21:38 +0530

    Time: 3 ms (63.11%), Space: 11.3 MB (69.02%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -4,6 +4,7 @@ public:
         stack<char> st;
         string ans = "";
         if(k==num.size()) return "0";
+        //0(n)
         for(int i=0;i<num.length();i++){
             while(!st.empty()&& k>0 && ((st.top()-'0') > (num[i] -'0'))){
                 k--;
@@ -11,15 +12,18 @@ public:
             }
             st.push(num[i]);
         }
+        //0(k)
         while(k>0){
               st.pop();
               k--;
         }
+        //0(n)
         while(!st.empty()){
             ans.push_back(st.top());
             st.pop();
         }
         //if stack mai sirf 0s the , iss loop ke baaad saare 0 nikal jayenge 
+    //    0(n)-> reversal and removing zero 
         while(ans.size()!=0 && ans.back()=='0'){
             ans.pop_back();
         }
