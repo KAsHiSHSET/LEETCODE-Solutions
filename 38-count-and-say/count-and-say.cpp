3be916bc1909fdfd7b7f6a59da9a19d4496b272a
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2025-10-03 13:06:52 +0530

    Time: 59 ms (5.08%) | Memory: 125.4 MB (5.23%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
- //tc - 0(2^n)
- //sc -0(2^n) + 0(n)
-    string sayfunction(int n){
-        if(n==1){
-            return "1";
-        }
-        string result="";
-        string say = sayfunction(n-1);
-        //har step par aake processing 
-        for(int i=0;i<say.length();i++){
-            char ch= say[i];
-            int count=1;
-            while(i<say.length()-1&&say[i]==say[i+1]){
-              count++;
-              i++;
-            }
-            result+= to_string(count)+ string(1,ch);
-
-        }
-        return result;    
-    }
+    
     string countAndSay(int n) {
-        return sayfunction(n);
+        if(n==1) return "1";
+        string ans= countAndSay(n-1);
+        string result="";
+        int count;
+        for(int i=0;i<ans.length();i++){
+            char ch= ans[i];
+            count=1;
+            while(i<ans.length()-1 && ans[i]==ans[i+1]){
+                count++; i++;
+            }
+            result =result + to_string(count) + string(1,ch);
+        }
+        return result;
     }
 };
