38c382b1cc751690c09bd6fa38b46db16c753e39
Kashish <139335575+KAsHiSHSET@users.noreply.github.com>
2026-06-11 13:59:40 +0530

    Time: 43 ms (57.49%) | Memory: 50.9 MB (19.83%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,38 +1,15 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-     //we dont have to calculate the max len but the total number of arrayss with sum k
-     int l=0;
-     int r=0;
-     int sum=0;
-     int count=0;
-     int n=nums.size();
-     int maxlen=0;
-    //  while(r<n){
-    //     while(l<=r && sum > k){
-    //         sum=sum-nums[l];
-    //         l++;
-    //     }
-    //     if(sum==k){
-    //         maxlen=max(maxlen,r-l+1);
-    //         count++;
-    //     }
-    //     if(r<n) sum+=nums[r++];
-    //  }
-    //  return count;
-    // }
-    unordered_map<int,int> mp;
-    //prefixsum+ hashmap
-
-    for(int i=0;i<n;i++){
-         sum=sum+nums[i];
-         if(sum==k) count++;
-         if(mp.find(sum-k)!=mp.end()){
-            count+= mp[sum-k];
-         }
-         mp[sum]++;
-         
-    }
-    return count;
+        unordered_map<int,int> mpp;
+        int presum=0 ; int cnt =0;
+        mpp[0]=1;
+        for(int i =0;i<nums.size();i++){
+            presum+= nums[i];
+            int remove = presum - k;
+            cnt += mpp[remove];
+            mpp[presum] += 1;
+        }
+        return cnt;
     }
 };
